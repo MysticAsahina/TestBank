e60fbcc5bff203851fc310eba9dad8bedded4a32
MysticAsahina <mysticasahina@gmail.com>
2025-10-05 10:47:57 +0800

    Update Admin.js

--- Admin.js
+++ Admin.js
@@ -1,10 +1,1 @@
-const mongoose = require("mongoose");
 
-const adminSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-  adminId: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  email: { type: String, required: true },
-});
-
-module.exports = mongoose.model("Admin", adminSchema);
