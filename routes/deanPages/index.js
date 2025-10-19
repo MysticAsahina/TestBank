87d87e6e350276793489640feb588dda193f75d6
MysticAsahina <mysticasahina@gmail.com>
2025-10-19 11:17:39 +0800

    Add sections and test-static routes to deanPages

--- routes/deanPages/index.js
+++ routes/deanPages/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dashboardRoutes from "./dashboard.js";
 import manageAccountsRoutes from "./manageAccounts.js";
+import sectionsRoutes from "./sections.js";
 import testManagementRoutes from "./testManagement.js";
+import testStaticRoutes from "./testS.js";
 import studentPerformanceRoutes from "./studentPerformance.js";
 import reportsRoutes from "./reports.js";
 import myAccountsRoutes from "./myAccounts.js";
@@ -10,7 +12,9 @@ const router = express.Router();
 
 router.use("/dashboard", dashboardRoutes);
 router.use("/manage-accounts", manageAccountsRoutes);
+router.use("/sections", sectionsRoutes);
 router.use("/tests", testManagementRoutes);
+router.use("/test-static", testStaticRoutes);
 router.use("/student-performance", studentPerformanceRoutes);
 router.use("/reports", reportsRoutes);
 router.use("/account", myAccountsRoutes);
